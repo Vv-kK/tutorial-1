98624d0c2148ce4b261f3e256e9129b283fc28f4
Vv-kK <veronicakylie1@gmail.com>
2024-02-14 12:31:25 +0700

    fix return string controller

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -16,14 +16,14 @@ public class ProductController {
 
     @GetMapping("")
     public String Home(Model model){
-        return "home";
+        return "Home";
     }
 
     @GetMapping("/product/create")
     public String createProductPage(Model model){
         Product product = new Product();
         model.addAttribute("product", product);
-        return "createProduct";
+        return "CreateProduct";
     }
 
     @PostMapping("/product/create")
@@ -36,7 +36,7 @@ public class ProductController {
     public String editProductPage(@PathVariable String productId, Model model){
         Product product = service.getProductById(productId);
         model.addAttribute("product", product);
-        return "editProduct";
+        return "EditProduct";
     }
 
     @PostMapping("/product/edit/save")
@@ -55,6 +55,6 @@ public class ProductController {
     public String productListPage(Model model){
         List<Product> allProducts = service.findAll();
         model.addAttribute("products", allProducts);
-        return "productList";
+        return "ProductList";
     }
 }
