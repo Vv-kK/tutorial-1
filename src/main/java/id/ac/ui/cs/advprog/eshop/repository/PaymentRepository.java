219074e0a1e55cdadba9d3e2af321882f562499b
Vv-kK <veronicakylie1@gmail.com>
2024-03-06 00:36:07 +0700

    [GREEN] Implement PaymentRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -10,14 +10,29 @@ public class PaymentRepository {
     private List<Payment> payments = new ArrayList<>();
 
     public Payment save(Payment payment) {
-        return null;
+        int i = 0;
+        for (Payment savedPayment : payments) {
+            if (savedPayment.getId().equals(payment.getId())) {
+                payments.remove(i);
+                payments.add(i, payment);
+                return payment;
+            }
+            i += 1;
+        }
+        payments.add(payment);
+        return payment;
     }
 
     public Payment findById(String id) {
+        for (Payment payment : payments) {
+            if (payment.getId().equals(id)) {
+                return payment;
+            }
+        }
         return null;
     }
 
     public List<Payment> getAllPayment() {
-        return null;
+        return payments;
     }
 }
